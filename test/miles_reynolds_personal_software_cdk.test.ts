6cb44378815c117b81334bfbedda7c6a2bfb2796
Miles Reynolds <yh643@georgetown.edu>
2023-08-13 22:56:59 -0400

    Initial commit

---
+++ test/miles_reynolds_personal_software_cdk.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as MilesReynoldsPersonalSoftwareCdk from '../lib/miles_reynolds_personal_software_cdk-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/miles_reynolds_personal_software_cdk-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new MilesReynoldsPersonalSoftwareCdk.MilesReynoldsPersonalSoftwareCdkStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
